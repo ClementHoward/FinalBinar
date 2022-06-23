890dd1b6084371eecb21f310e124988a71b24579
Clement <clmnthwrd@gmail.com>
2022-06-23 21:24:44 +0700

    reg berhasil

--- src/main/java/Final/Project/Binar/Final/Project/Binar/Controller/LoginController.java
+++ src/main/java/Final/Project/Binar/Final/Project/Binar/Controller/LoginController.java
@@ -32,7 +32,7 @@ public class LoginController
         {
             userLoginServiceImpl.saveUser(user);
         }
-        return  new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>("registrasi berhasil", HttpStatus.CREATED);
     }
 
     @PostMapping("/registration-seller")
